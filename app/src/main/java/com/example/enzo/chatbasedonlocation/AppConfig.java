779dfe3d6ff1afb51358745de9ce899359b6c4ee
Enzo Licul <enzo.licul@yahoo.com>
2017-05-14 01:59:56 +0200

    First commit

---
+++ app/src/main/java/com/example/enzo/chatbasedonlocation/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.enzo.chatbasedonlocation;
+
+/**
+ * Created by Enzo on 12.5.2017..
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://locationchatroulette.com/public/api/auth/login";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://locationchatroulette.com/public/api/auth/signup";
+}
