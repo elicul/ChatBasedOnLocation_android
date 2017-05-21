b73ec459861ecc511deeda8a9b4ef225cb0a28e5
Enzo Licul <enzo.licul@yahoo.com>
2017-05-21 17:28:05 +0200

    Logging working with firebase and facebook

--- app/src/main/java/com/example/enzo/chatbasedonlocation/MainActivity.java
+++ app/src/main/java/com/example/enzo/chatbasedonlocation/MainActivity.java
@@ -5,214 +5,93 @@ package com.example.enzo.chatbasedonlocation;
  */
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.facebook.login.widget.ProfilePictureView;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.facebook.login.LoginManager;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private static final String TAG = "FacebookLogin";
+    private ShareDialog shareDialog;
+    private Button logout;
 
-    CallbackManager callbackManager;
-    TextView FacebookDataTextView;
-    LoginButton loginButton;
-    AccessTokenTracker accessTokenTracker;
-    ProfilePictureView profilePictureView;
-    String FacebookUserID ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
-        // Passing MainActivity in Facebook SDK.
-        FacebookSdk.sdkInitialize(MainActivity.this);
-
-        // Adding Callback Manager.
-        callbackManager = CallbackManager.Factory.create();
-
+        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Assign TextView ID.
-        FacebookDataTextView = (TextView)findViewById(R.id.TextView1);
+        shareDialog = new ShareDialog(this);
 
-        // Assign Facebook Login button ID.
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-
-        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePic);
-
-        // Giving permission to Login Button.
-        loginButton.setReadPermissions("email", "public_profile");
-
-        // Checking the Access Token.
-        if(AccessToken.getCurrentAccessToken()!=null){
-
-            GraphLoginRequest(AccessToken.getCurrentAccessToken());
-
-            // If already login in then show the Toast.
-            Toast.makeText(MainActivity.this,"Already logged in",Toast.LENGTH_SHORT).show();
-
-        }else {
-
-            // If not login in then show the Toast.
-            Toast.makeText(MainActivity.this,"User not logged in",Toast.LENGTH_SHORT).show();
-        }
-
-        // Adding Click listener to Facebook login button.
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>(){
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult){
-                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                // Calling method to access User Data After successfully login.
-                //GraphLoginRequest(loginResult.getAccessToken());
-                handleFacebookAccessToken(loginResult.getAccessToken());
+            public void onClick(View view) {
+                ShareLinkContent content = new ShareLinkContent.Builder().build();
+                shareDialog.show(content);
             }
-
-            @Override
-            public void onCancel(){
-                Log.d(TAG, "facebook:onCancel");
-                Toast.makeText(MainActivity.this,"Login Canceled",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(FacebookException exception){
-                Log.d(TAG, "facebook:onError", exception);
-                Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
-            }
-
         });
 
-        // Detect user is login or not. If logout then clear the TextView and delete all the user info from TextView.
-        accessTokenTracker = new AccessTokenTracker() {
+        Bundle inBundle = getIntent().getExtras();
+        String name = inBundle.get("name").toString();
+        String surname = inBundle.get("surname").toString();
+        String imageUrl = inBundle.get("imageUrl").toString();
+
+        TextView nameView = (TextView)findViewById(R.id.nameAndSurname);
+        nameView.setText("" + name + " " + surname);
+        Button logout = (Button)findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onCurrentAccessTokenChanged(AccessToken accessToken, AccessToken accessToken2) {
-                if (accessToken2 == null) {
-
-                    // Clear the TextView after logout.
-                    FacebookDataTextView.setText("");
-
-                    // Clear the profilePictureView after logout.
-                    profilePictureView.setProfileId(null);
-
-                }
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+                finish();
             }
-        };
+        });
+        new MainActivity.DownloadImage((ImageView)findViewById(R.id.profileImage)).execute(imageUrl);
     }
 
-    // Method to access Facebook User Data.
-    protected void GraphLoginRequest(AccessToken accessToken){
-        GraphRequest graphRequest = GraphRequest.newMeRequest(accessToken,
-                new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+    public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
 
-                        try {
+        public DownloadImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
 
-                            // Getting Facebook User ID into variable.
-                            FacebookUserID = jsonObject.getString("id");
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
 
-                            // Adding Facebook User Full name into TextView.
-                            FacebookDataTextView.setText(FacebookDataTextView.getText() + "\nName : " + jsonObject.getString("name"));
-
-                            // Adding Facebook User Email into TextView.
-                            FacebookDataTextView.setText(FacebookDataTextView.getText() + "\nEmail : " + jsonObject.getString("email"));
-
-                            // BY using FacebookUserID call profile pic from facebook server.
-                            profilePictureView.setProfileId(FacebookUserID);
-                        }
-                        catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-
-        Bundle bundle = new Bundle();
-        bundle.putString(
-                "fields",
-                "id,name,email"
-        );
-        graphRequest.setParameters(bundle);
-        graphRequest.executeAsync();
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        AppEventsLogger.activateApp(MainActivity.this);
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        AppEventsLogger.deactivateApp(MainActivity.this);
-
-    }
-
-    private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
-
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(MainActivity.this,"Login Success",Toast.LENGTH_SHORT).show();
-
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-                    }
-                });
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 
 }
