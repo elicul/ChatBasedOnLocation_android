86565834b96922895bc7e95f318ba92c9cabf46d
Enzo Licul <enzo.licul@yahoo.com>
2017-05-21 16:42:47 +0200

    Logining working

--- app/src/main/java/com/example/enzo/chatbasedonlocation/MainActivity.java
+++ app/src/main/java/com/example/enzo/chatbasedonlocation/MainActivity.java
@@ -6,41 +6,213 @@ package com.example.enzo.chatbasedonlocation;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "FacebookLogin";
+
+    CallbackManager callbackManager;
+    TextView FacebookDataTextView;
+    LoginButton loginButton;
+    AccessTokenTracker accessTokenTracker;
+    ProfilePictureView profilePictureView;
+    String FacebookUserID ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        // Passing MainActivity in Facebook SDK.
+        FacebookSdk.sdkInitialize(MainActivity.this);
+
+        // Adding Callback Manager.
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_main);
 
+        // Assign TextView ID.
+        FacebookDataTextView = (TextView)findViewById(R.id.TextView1);
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
+        // Assign Facebook Login button ID.
+        loginButton = (LoginButton)findViewById(R.id.login_button);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePic);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // Giving permission to Login Button.
+        loginButton.setReadPermissions("email", "public_profile");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Checking the Access Token.
+        if(AccessToken.getCurrentAccessToken()!=null){
+
+            GraphLoginRequest(AccessToken.getCurrentAccessToken());
+
+            // If already login in then show the Toast.
+            Toast.makeText(MainActivity.this,"Already logged in",Toast.LENGTH_SHORT).show();
+
+        }else {
+
+            // If not login in then show the Toast.
+            Toast.makeText(MainActivity.this,"User not logged in",Toast.LENGTH_SHORT).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        // Adding Click listener to Facebook login button.
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>(){
+            @Override
+            public void onSuccess(LoginResult loginResult){
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                // Calling method to access User Data After successfully login.
+                //GraphLoginRequest(loginResult.getAccessToken());
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel(){
+                Log.d(TAG, "facebook:onCancel");
+                Toast.makeText(MainActivity.this,"Login Canceled",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception){
+                Log.d(TAG, "facebook:onError", exception);
+                Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        // Detect user is login or not. If logout then clear the TextView and delete all the user info from TextView.
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken accessToken, AccessToken accessToken2) {
+                if (accessToken2 == null) {
+
+                    // Clear the TextView after logout.
+                    FacebookDataTextView.setText("");
+
+                    // Clear the profilePictureView after logout.
+                    profilePictureView.setProfileId(null);
+
+                }
+            }
+        };
     }
+
+    // Method to access Facebook User Data.
+    protected void GraphLoginRequest(AccessToken accessToken){
+        GraphRequest graphRequest = GraphRequest.newMeRequest(accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+
+                        try {
+
+                            // Getting Facebook User ID into variable.
+                            FacebookUserID = jsonObject.getString("id");
+
+                            // Adding Facebook User Full name into TextView.
+                            FacebookDataTextView.setText(FacebookDataTextView.getText() + "\nName : " + jsonObject.getString("name"));
+
+                            // Adding Facebook User Email into TextView.
+                            FacebookDataTextView.setText(FacebookDataTextView.getText() + "\nEmail : " + jsonObject.getString("email"));
+
+                            // BY using FacebookUserID call profile pic from facebook server.
+                            profilePictureView.setProfileId(FacebookUserID);
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+        Bundle bundle = new Bundle();
+        bundle.putString(
+                "fields",
+                "id,name,email"
+        );
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(MainActivity.this);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(MainActivity.this);
+
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this,"Login Success",Toast.LENGTH_SHORT).show();
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+
 }
