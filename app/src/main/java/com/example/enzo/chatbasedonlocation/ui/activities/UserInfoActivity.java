bb91ce988ab5b9b0251b1c3a43074ed91dd60117
Enzo Licul <enzo.licul@yahoo.com>
2017-06-09 03:04:24 +0200

    Izbrisane nepotrebne stvari, sitno preradjeni settingsi

---
+++ app/src/main/java/com/example/enzo/chatbasedonlocation/ui/activities/UserInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.enzo.chatbasedonlocation.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.enzo.chatbasedonlocation.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
